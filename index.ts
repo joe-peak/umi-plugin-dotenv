e918d323d9a10470a03a9288de5c2003be9a7b86
joe <joe@fiture.com>
2022-06-29 15:21:33 +0800

    feat: 添加插件逻辑

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { join } from 'path'
+import { IApi } from 'umi';
+import DotEnv from 'dotenv-webpack'
+
+export default function dotEnv(api: IApi) {
+  const deployEnv = process.env.DEPLOY_ENV || 'dev';
+
+  api.describe({
+    key: 'dotEnv',
+    config: {
+      default: {
+        path: join(api.cwd, './config', `./.env.${deployEnv}`)
+      },
+      schema(joi){
+        return joi.object({
+          path: joi.string()
+        });
+      },
+    },
+    enableBy: api.EnableBy.register,
+  })
+
+  api.chainWebpack(memo => {
+    memo.plugin('dotEnv').use(DotEnv, [{ path: api.config.dotEnv?.path}]);
+    return memo;
+  });
+}
